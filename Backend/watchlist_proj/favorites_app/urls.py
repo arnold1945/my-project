806f4d4478ae6ecb6b7e3836ff3c34f7455cfb2a
arnold1945 <vscj58@gmail.com>
2025-12-11 13:37:05 -0600

    watchlist_proj, user_app, and favorites_app completed(?)

---
+++ Backend/watchlist_proj/favorites_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import MovieManager, ShowManager
+
+urlpatterns = [
+    # Movies
+    path('movies/<int:movie_id>/', MovieManager.as_view(), name='movie-detail'),
+    path('movies/<int:movie_id>/add/', MovieManager.as_view(), name='movie-add'),
+    path('movies/<int:movie_id>/remove/', MovieManager.as_view(), name='movie-remove'),
+
+    # Shows
+    path('shows/<int:show_id>/', ShowManager.as_view(), name='show-detail'),
+    path('shows/<int:show_id>/add/', ShowManager.as_view(), name='show-add'),
+    path('shows/<int:show_id>/remove/', ShowManager.as_view(), name='show-remove'),
+]
--- Backend/watchlist_proj/favorites_app/views.py
+++ Backend/watchlist_proj/favorites_app/views.py
@@ -1,19 +1,75 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+# from rest_framework import status
 from rest_framework import status as s
+
 from .models import Movie, Show, Favorites
 from user_app.models import Client
 from .serializers import MovieDetailSerializer, MovieListSerializer, ShowDetailSerializer, ShowListSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+import requests
 
 # Create your views here.
 
-class FavoritesManager(APIView):
+class MovieManager(APIView):
     permission_classes = [IsAuthenticated]
     
     def get(self, request, movie_id=None):
         if movie_id is not None:
-            movie = get_object_or_404()
+            movie = get_object_or_404(Movie, id=movie_id)
+            serializer = MovieDetailSerializer(movie)
+            return Response(serializer.data, status=s.HTTP_200_OK)
+    ## this
+    def post(self, request, movie_id=None):
+        user_favorites, _ = Favorites.objects.get_or_create(specific_user = request.user) 
+        
+        url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=YOUR_API_KEY'
+        response = requests.get(url)
+        data = response.json()
+        movie, created = Movie.objects.get_or_create(
+            api_id = data['id'],
+            defaults={'title': data['title']}
+        )
+        
+        
+        user_favorites.fav_movies.add(movie)
+        #checkpoint *status
+        return Response({'message': 'Movie added to favorites!'}, status=201)
+        
+    def delete(self, request, movie_id=None):
+        user_favorites= Favorites.objects.get(specific_user=request.user)
+        movie = get_object_or_404(Movie, id=movie_id)
+        user_favorites.fav_movies.remove(movie)
+        return Response({'message':'Movie removed from favorites'}, status=204)
+
+class ShowManager(APIView):
+    permission_classes= [IsAuthenticated]
+
+    def get(self,request, show_id=None):
+        if show_id is not None:
+            show = get_object_or_404(Show, id=show_id)
+            serializer = ShowDetailSerializer(show)
+            return Response(serializer.data, status=s.HTTP_200_OK)
+    def post(self, request, show_id=None):
+        user_favorites, _ = Favorites.objects.get_or_create(specific_user = request.user) 
+        
+        url = f'https://api.tvmaze.com/shows/{show_id}'
+        response= requests.get(url)
+        data = response.json()
+        
+        show, created = Show.objects.get_or_create(
+            api_id = data['id'],
+            defaults={'title': data['name']}
+        )
+        
+        
+        user_favorites.fav_shows.add(show)
+        return Response({'message': 'Show added to favorites!'}, status=201)
     
+    def delete(self, request, show_id=None):
+        user_favorites= Favorites.objects.get(specific_user=request.user)
+        show = get_object_or_404(Show, id=show_id)
+        user_favorites.fav_shows.remove(show)
+        return Response({'message':'Show removed from favorites'}, status=204)
