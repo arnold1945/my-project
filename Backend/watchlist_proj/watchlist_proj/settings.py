99302f774f555a30f9634947a3f839fbc15f9e06
arnold1945 <vscj58@gmail.com>
2025-12-16 14:58:01 -0600

    test

--- Backend/watchlist_proj/watchlist_proj/settings.py
+++ Backend/watchlist_proj/watchlist_proj/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'favorites_app',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'watchlist_proj.urls'
 
--- frontend/watchlist-frontend/src/components/MoviesPage.jsx
+++ frontend/watchlist-frontend/src/components/MoviesPage.jsx
@@ -4,3 +4,38 @@ import axios from "axios";
 import { useOutletContext } from "react-router-dom";
 
 
+export default function MoviesPage() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+
+    const getMovies = async () => {
+      try{
+        const response1 = await axios.get(
+          "http://127.0.0.1:8000/movies/popular/"
+        );
+        setMovies(response1.data)
+      } catch (err) {
+        console.error("Error getting movies from backend", err);
+
+      }
+    };
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Movies</h1>
+
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            {movie.title} ({movie.year})
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+
+
+}
